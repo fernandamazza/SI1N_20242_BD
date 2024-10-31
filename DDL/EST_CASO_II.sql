6264e5f691fd288d97629789a2ae5c71a699d509
Fernanda <fmazzalima@gmail.com>
2024-10-31 15:58:35 -0300

    Add files via upload

---
+++ DDL/EST_CASO_II.sql
@@ -0,0 +1,67 @@
+-- Active: 1728688414725@@127.0.0.1@3306@est_caso_ii
+#CREATE SCHEMA Est_Caso_II;
+
+
+use Est_Caso_II;
+
+CREATE TABLE IF NOT EXISTS Voos(
+    Voo_Codigo INT PRIMARY KEY,
+    data_Hora_Origem DATETIME NOT NULL,
+    data_Hora_Destino DATETIME NOT NULL,
+    aeroporto_Origem VARCHAR(50) NOT NULL,
+    aeroporto_Destino VARCHAR(50) NOT NULL,
+    aeronave_Prefixo VARCHAR(5),
+    CONSTRAINT Aeronave_FK FOREIGN KEY(Aeronave_Prefixo)
+    REFERENCES Aeronaves(aeronave_Prefixo)
+);
+
+CREATE TABLE IF NOT EXISTS Aeronaves(
+    aeronave_Prefixo VARCHAR(10) PRIMARY KEY,
+    aeronave_Modelo VARCHAR(45) NOT NULL,
+    aeronave_anofabri VARCHAR(45) NOT NULL,
+    aeronave_capacidade VARCHAR(45) NOT NULL,
+    aeronave_Fabricante VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Aeroportos(
+    aeroporto_Cod VARCHAR(3) PRIMARY KEY,
+    aeroporto_Nome VARCHAR(45) NOT NULL,
+    aeroporto_Local VARCHAR(50) NOT NULL,
+    aeroporto_Pais VARCHAR(50) NOT NULL,
+    aeroporto_Longitude FLOAT NOT NULL,
+    aeroporto_Latitude FLOAT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Reservas(
+    FK_Voo INT,
+    FK_Pass VARCHAR(11),
+    CONSTRAINT voo_fk FOREIGN KEY (FK_Voo)
+    REFERENCES Voos(Voo_Codigo),
+    CONSTRAINT passageros_fk FOREIGN KEY(FK_Pass)
+    REFERENCES Passageiros(pass_CPF)
+);
+
+CREATE TABLE IF NOT EXISTS Passageiros(
+    pass_CPF VARCHAR(11) PRIMARY KEY,
+    pass_Nome VARCHAR(45) NOT NULL,
+    pass_Tel VARCHAR(13) NOT NULL,
+    pass_End VARCHAR(100) NOT NULL,
+    pass_Email VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Funcionarios(
+    func_ID INT PRIMARY KEY,
+    func_Funcao VARCHAR(30) NOT NULL,
+    func_Nome VARCHAR(45) NOT NULL,
+    func_Data_Nasc DATE NOT NULL,
+    func_Tel VARCHAR(13) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Operam_Voo(
+    Func_FK INT,
+    Voos_FK INT,
+    CONSTRAINT funcionarios_FK FOREIGN KEY(Func_FK)
+    REFERENCES Funcionarios(func_ID),
+    CONSTRAINT voos_FK FOREIGN KEY(Voos_FK)
+    REFERENCES Voos(Voo_Codigo)
+);
